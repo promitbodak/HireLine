e356b7784935b68660b997fea240f136cd4592aa
Promit Bodak <113660151+promitbodak@users.noreply.github.com>
2025-04-26 10:04:09 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -204,7 +204,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "Strict",
+        sameSite: "None",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
