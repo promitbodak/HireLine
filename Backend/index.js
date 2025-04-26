cea2681a1b99e40d3d8f62d4b0fcafd3ea7ea948
Promit Bodak <113660151+promitbodak@users.noreply.github.com>
2025-04-26 08:17:04 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://hireline-1.onrender.com"],
+  origin: true,
   credentials: true,
 };
 
